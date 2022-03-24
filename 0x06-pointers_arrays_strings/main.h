f5712ab3d258af60ad69cc4d03735d4fb06d523c
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-03-24 00:43:32 -0700

    stract

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <math.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -19,4 +16,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-#endif /* MAIN_H */~
+#endif
