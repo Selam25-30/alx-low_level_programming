1c0beb294c5a442294639a7a9593da735ed52f88
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-04-11 15:33:47 -0700

    structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(char *s)
 }
 
 /**
-display * *_strcpy - copies the string pointed to by src
+ * display * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
