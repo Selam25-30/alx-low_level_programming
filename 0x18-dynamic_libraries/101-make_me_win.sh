66e3108550b4772ff0af5a92e210f6884af78c13
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-06-14 10:23:18 -0700

    lib.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.comSelam25-30/alx-low_level_programming/main/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+gcc *.o -fPIC -shared -o libruin.so
